a620f67b8b304f890c37ceabc9af9a097bb7b288
☾ Lu <72708201+Lu-LuToine@users.noreply.github.com>
2024-11-02 23:47:41 +0100

    add color and name of game as ascii code

--- main.cpp
+++ main.cpp
@@ -15,9 +15,22 @@ void setConsoleColor(int color) {
     SetConsoleTextAttribute(hConsole, color);
 }
 
+void startGameName() {
+    setConsoleColor(14);
+    cout << ">>==========================================================<<" <<endl;
+    cout << "||                                                          ||" <<endl;
+    cout << "|| _    ____ _   _ _ _  _ ____    ____ ____ ____ ____ ____  ||" <<endl;
+    cout << "|| |    |__|  \\_/  | |\\ | | __    | __ |__/ |__| [__  [__   ||" <<endl;
+    cout << "|| |___ |  |   |   | | \\| |__]    |__] |  \\ |  | ___] ___]  ||" <<endl;
+    cout << "||                        Console Game                      ||" <<endl;
+    cout << "||                                                          ||" <<endl;
+    cout << ">>==========================================================<<" <<endl;
+    setConsoleColor(7);
+}
 
 int main()
 {
+    startGameName();
     // [DEBUG] => check PLAYER and colors
     for (int i = 0; i < Player_number; i++) {
         if (i == 1) {
